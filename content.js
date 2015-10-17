d712fc3f32418416d8a1357497f8467a60ef955b
Martin Scavnicky <martin.scavnicky@gmail.com>
2015-10-17 19:46:11 +0200

    Display image while blocking website.

--- content.js
+++ content.js
@@ -10,9 +10,13 @@ chrome.runtime.sendMessage(message, function(response) {
   isUnblocked = response;
 
   if (!isUnblocked) {
-    console.log("Stopping page load.")
-    window.stop();
+    var body = document.createElement("body");
+    document.documentElement.appendChild(body);
+    document.body.innerHTML = '<div id="stop"><img src="http://robinberzinmd.com/wp-content/uploads/2014/05/mac-air-iphone-wood-table-pic-2-16x9.jpg"/></div>'; 
 
+    console.log("Stopping page load.");
+    window.stop();
+    
     setTimeout(function() {
       chrome.runtime.sendMessage({
         subject: "unblock",
