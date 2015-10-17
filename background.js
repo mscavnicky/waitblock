215b282f3988ddb26f96c0ca72713d4308d13643
Martin Scavnicky <martin.scavnicky@gmail.com>
2015-10-17 22:33:22 +0200

    Update site blocking to include blocklist and unblock periods.

--- background.js
+++ background.js
@@ -1,20 +1,53 @@
-var spinBlock = {}
+// Time to wait before unblocking the page. In milliseconds.
+var unblockTime = 15000;
+// Time to wait till the page is blocked after unblocking. In milliseconds.
+var blockTime = 5 * 60 * 1000;
 
-function isUnblocked(tabId, location) {
-  console.log("SpinBlock: " + JSON.stringify(spinBlock));
-  return spinBlock[tabId] === location;
+// The list of domains to block. Covers subdomains as well.
+var blockedDomains = [
+  'facebook.com',
+  'idnes.cz',
+  'pravda.sk'
+];
+
+// Hash of times in milliseconds when particular domain was blocked.
+var domainUnblockTimes = {};
+
+// Determine whether specific hostname is currently blocked. Domain is blocked
+// if it is in the blocklist and was not recently unblocked.
+function blocked(message) {
+  var domain =  _.find(blockedDomains, function (domain) {
+    return message.hostname.endsWith(domain);
+  });
+
+  if (_.isUndefined(domain)) {
+    return false;
+  }
+
+  var domainUnblockTime = domainUnblockTimes[domain];
+  if (_.isUndefined(domainUnblockTime)) {
+    return true;
+  }
+  console.log("Unblock time " + domainUnblockTime);
+
+  // Site is blocked again when the it was unblocked for too long.
+  console.log("Time since unblock " + (Date.now() - domainUnblockTime));
+  return Date.now() - domainUnblockTime > blockTime;
 }
 
-function unblock(tabId, location) {
-  spinBlock[tabId] = location;
+function unblock(message) {
+  var domain =  _.find(blockedDomains, function (domain) {
+    return message.hostname.endsWith(domain);
+  });
+
+  domainUnblockTimes[domain] = Date.now();
 }
 
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-  if (message.subject === "isUnblocked") {
-    console.log("Received isUnblocked message for tab " + sender.tab.id);
-    sendResponse(isUnblocked(sender.tab.id, message.location));
-  } else if (message.subject === "unblock") {
-    console.log("Received unblock message for tab " + sender.tab.id);
-    unblock(sender.tab.id, message.location)
-  }
+  console.log("Received message " + message.subject);
+
+  sendResponse({
+    'blocked': blocked,
+    'unblock': unblock
+  }[message.subject](message));
 });
--- content.js
+++ content.js
@@ -1,36 +1,36 @@
-var isUnblocked = false;
+function block() {
+  // In this stage of page lifecycle body element does
+  // not exist and we need to manually create it.
+  var body = document.createElement("body");
+  document.documentElement.appendChild(body);
+
+  $.ajax({
+    url: chrome.extension.getURL('template.html'),
+    success: function(html) {
+      document.body.innerHTML = html;
+      // Stop the original page from loading
+      window.stop();
+    }
+  });
+
+  setTimeout(function() {
+    chrome.runtime.sendMessage({
+      subject: "unblock",
+      hostname: window.location.hostname
+    });
+
+    window.location.reload();
+  }, 5000);
+}
+
 
 var message = {
-  subject: "isUnblocked",
-  location: window.location.hostname
+  subject: "blocked",
+  hostname: window.location.hostname
 };
 
 chrome.runtime.sendMessage(message, function(response) {
-  console.log("isUnblocked returned " + response);
-  isUnblocked = response;
-
-  if (!isUnblocked) {
-    // In this stage of page lifecycle body element does 
-    // not exist and we need to manually create it.
-    var body = document.createElement("body");
-    document.documentElement.appendChild(body);
-
-    $.ajax({
-      url: chrome.extension.getURL('template.html'),
-      success: function(html) {
-        document.body.innerHTML = html;
-        // Stop the original page from loading
-        window.stop();
-      }
-    });
-
-    setTimeout(function() {
-      chrome.runtime.sendMessage({
-        subject: "unblock",
-        location: window.location.hostname
-      });
-
-      window.location.reload()
-    }, 5000);
+  if (response === true) {
+    block();
   }
 });
